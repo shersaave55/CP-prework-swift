4c7533e13fb321b442d18e437126ee24d5d88a7e
shersaave55 <ssaavedra27@amherst.edu>
2025-04-28 14:08:16 -0400

    Initial Commit

---
+++ IOS_Prework_CPTests/IOS_Prework_CPTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Prework_CPTests.swift
+//  IOS_Prework_CPTests
+//
+//  Created by Sherlyn Saavedra on 4/28/25.
+//
+
+import Testing
+@testable import IOS_Prework_CP
+
+struct IOS_Prework_CPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
